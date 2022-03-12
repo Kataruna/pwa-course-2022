2cf3d9fa5ef4de0b3458b2c6cc6d5127e8222a7a
Kataruna <tuaa1bb2@gmail.com>
2022-03-12 16:37:35 +0700

    1630702288

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -3,15 +3,19 @@ import '../css/Userlist.css';
 
 function Userlist () {
   const [trayStatus, setTrayStatus] = useState("open");
+  const [operate, setOperate] = useState("x");
 
   function onButtonClicked() {
     if(trayStatus === "closed")  setTrayStatus("open");
     else if(trayStatus === "open") setTrayStatus("closed");
+
+    if(operate === "x") setOperate("o");
+    else if(operate === "o") setOperate("x");
   }
 
   return (
     <div className={"userlist "+trayStatus}>
-      <div className="close-button" onClick={onButtonClicked}>x</div>
+      <div className="close-button" onClick={onButtonClicked}>{operate}</div>
     </div>
   )
 }
